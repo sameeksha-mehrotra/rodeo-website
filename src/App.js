5ffc48975f8f8397e2b07b3517aead78fe6bd32a
sameeksha-mehrotra <saasha.mehrotra@gmail.com>
2025-07-22 21:51:39 -0500

    homePage changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import logo from './RaasRodeo11V3.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+       <div className="App-topPage">
+
+        <img src={logo} className="App-logo" alt="raas rodeo logo" />
+        
+        <rectangle className="App-topPage-rectangle">
+          
+        </rectangle>
+        
+       </div>
+        
+     <h1>RAAS RODEO XV</h1>
+
     </div>
   );
 }
+
 
 export default App;
