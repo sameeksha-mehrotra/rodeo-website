371c2e73b6908b3b2981dfe4f9c71d0f56e36fbb
sameeksha-mehrotra <saasha.mehrotra@gmail.com>
2025-07-25 13:21:59 -0500

    updates css social icons

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import logo from './RaasRodeo11V3.png';
+import insta from './instagram icon logo.png';
+import tiktok from './tiktok icon logo.png';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
 
+      {/*Top Page with Social Media Icons and black bar as the menue bar*/}
        <div className="App-topPage">
-
-        <img src={logo} className="App-logo" alt="raas rodeo logo" />
         
         <rectangle className="App-topPage-rectangle">
-          
+          <img src={logo} className="App-logo" alt="raas rodeo logo" />
+
+          <img src={tiktok} className="App-tiktok" alt="instagram logo" />
+
+          <img src={insta} className="App-insta" alt="instagram logo" />
         </rectangle>
         
        </div>
-        
-     <h1>RAAS RODEO XV</h1>
+
+      {/*Home Page*/}
+       <div className="App-homePage">
+        <h1>RAAS RODEO XV</h1>
+      </div>
 
     </div>
   );
