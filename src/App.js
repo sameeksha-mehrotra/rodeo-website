c5ccb5ba9e9ea318ea56bd088275d32d246fd9a7
sameeksha-mehrotra <saasha.mehrotra@gmail.com>
2025-07-26 20:24:08 -0500

    carousel add board mem pics

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import tisha from './tishaPic.png';
 import pavi from './paviPic.png';
 import entireBoard from './entireBoard.png';
 
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import headL from './headLiasons pic.png';
+
+
 import './App.css';
 
 function App() {
@@ -47,10 +52,10 @@ function App() {
            <h2>X V</h2>
         </h1>
 
-      <div className="gotham-frame">
-        <img className="App-entireBoard" src={entireBoard} alt="entire board" />
-      </div>
-      
+        <div className="gotham-frame">
+          <img className="App-entireBoard" src={entireBoard} alt="entire board" />
+        </div>
+
       </div>
 
       {/*About Section*/}
@@ -76,16 +81,16 @@ function App() {
 
       {/*Board Section*/}
       <div className="App-boardSection" id='board'>
-        <h1 class="boardTitle">Board</h1>
-        <rectangle className="boardSection-rectangle">
-          <p>
-            Raas Rodeo is a non-profit organization that aims to promote and celebrate the rich <b>cultural heritage</b> of Raas, a traditional Indian dance form. Our mission is to provide a platform for dancers and enthusiasts to come together, learn, and showcase their skills while fostering <b>a sense of community and cultural appreciation</b>.
-          </p>
-          <p>            We organize <b>workshops, performances, and events</b> that not only highlight the beauty of Raas but also educate people about its history and significance. Through our initiatives, we strive to create a vibrant community that<b>embraces diversity and encourages artistic expression</b>.
-          </p>
-          <p>            <b>Join us</b> in our journey to keep the spirit of Raas alive and thriving, as we bring together dancers, artists, and cultural enthusiasts from all walks of life. Whether you are a seasoned performer or someone new to the dance form, <b>Raas Rodeo welcomes you</b> to be a part of our community and celebrate the joy of Raas together.
-          </p>
-        </rectangle>  
+        <h1 className="boardTitle">Board</h1>
+
+        <Carousel className="boardCarousel" showThumbs={false} infiniteLoop autoPlay>
+          <div>
+            <img src={headL} alt="Head Liasons" />
+            <p className="legend">cjebcfjsb</p>
+          </div>
+          {/* Add more <div> slides as needed */}
+        </Carousel>
+        
       </div>
 
       {/*Partnerships Section*/}
