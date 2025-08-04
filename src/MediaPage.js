25883451a62de5dee1f50e2afde1d90730e764f7
sameeksha-mehrotra <saasha.mehrotra@gmail.com>
2025-08-04 17:22:07 -0500

    media page style

--- src/MediaPage.js
+++ src/MediaPage.js
@@ -4,6 +4,11 @@ import './App.css';
 import raasAM from './wreckinRaas_TeamLogo.PNG';
 import washRaas from './washURaasLogo.png';
 import uwRaas from './uWRaasLogo.png';
+import ucbRaas from './UCBLogo.png';
+import steelRaas from './steelCityRaas.png';
+import columbiaRaas from './columbiaRaasLogo.png';
+import gtRaas from './GTRR-Logo.jpeg';
+import taraas from './tarassLogo.png';
 
 function MediaPage() {
   return (
@@ -36,6 +41,50 @@ function MediaPage() {
           </div>
 
         </div>
+
+        <div className="teamsRight">
+
+          <div className="teamFrameFive">
+            <img className="teamImage" src={ucbRaas} alt="Team 6"/>
+            <div className="overlay">
+              <span className="overlayText">Team 6</span>
+            </div>
+          </div>
+
+          <div className="teamFrame">
+            <img className="teamImage" src={steelRaas} alt="Team 7"/>
+            <div className="overlay">
+              <span className="overlayText">Team 7</span>
+            </div>
+          </div>
+
+          <div className="teamFrameFive">
+            <img className="teamImage" src={columbiaRaas} alt="Team 8"/>
+            <div className="overlay">
+              <span className="overlayText">Team 8</span>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="teamsCenter">
+
+          <div className="teamFramePlacementFirst">
+            <img className="teamImage" src={gtRaas} alt="First Place"/>
+            <div className="overlay">
+              <span className="overlayText">First Place</span>
+            </div>
+          </div>
+
+          <div className="teamFramePlacement">
+            <img className="teamImage" src={taraas} alt="Second Place"/>
+            <div className="overlay">
+              <span className="overlayText">Second Place</span>
+            </div>
+          </div>
+
+        </div>
+
     </div>
   );
 }
