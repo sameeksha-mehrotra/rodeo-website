51eb3a9e9854da15f322bbe24aabbf93975c714e
sameeksha-mehrotra <saasha.mehrotra@gmail.com>
2025-08-08 00:47:33 -0500

    media card change

--- src/MediaPage.js
+++ src/MediaPage.js
@@ -24,6 +24,7 @@ function MediaPage() {
             <img className="teamImage" src={gtRaas} alt="First Place" onClick={() => window.open('https://youtu.be/BVLQoistZlQ?si=zdj55TL_Z-_bbHIH', '_blank')}/>
             <div className="overlay">
               <span className="overlayText">First Place</span>
+              <span className="overlayTextName">Georgia Tech Ramblin' Raas</span>
             </div>
           </div>
 
@@ -31,13 +32,15 @@ function MediaPage() {
             <img className="teamImage" src={taraas} alt="Second Place" onClick={() => window.open('https://youtu.be/aQQ4JB4apHk?si=583LOQBUinVUeoc9', '_blank')}/>
             <div className="overlay">
               <span className="overlayText">Second Place</span>
+              <span className="overlayTextName">University of Texas at Dallas TaRaas</span>
             </div>
           </div>
 
           <div className="teamFrameFive">
-            <img className="teamImage" src={ucbRaas} alt="Team 6" onClick={() => window.open('https://youtu.be/uVIZPXv-hsI?si=i6_PyxDnKoTB9vEW', '_blank')}/>
+            <img className="teamImage" src={ucbRaas} alt="Third Place" onClick={() => window.open('https://youtu.be/uVIZPXv-hsI?si=i6_PyxDnKoTB9vEW', '_blank')}/>
             <div className="overlay">
               <span className="overlayText">Third Place</span>
+              <span className="overlayTextName">UC Berkeley Raas Ramzat</span>
             </div>
           </div>
 
@@ -47,24 +50,27 @@ function MediaPage() {
 
           <div className="teamFrame">
             <a href="https://www.youtube.com/watch?v=47rsMTNm95U&t=40s" target="_blank" rel="noreferrer"> 
-              <img className="teamImage" src={raasAM} alt="Team 3"/>
+              <img className="teamImage" src={raasAM} alt="Team 4"/>
             </a>
             <div className="overlay">
               <span className="overlayText">Team 4</span>
+              <span className="overlayTextName">Texas A&M Wreckin' Raas</span>
             </div>
           </div>
 
           <div className="teamFrame">
-            <img className="teamImage" src={washRaas} alt="Team 4" onClick={() => window.open('https://youtu.be/ef5TEpz37Dg?si=UzKE8fWIxaJ9cdzS', '_blank')}/>
+            <img className="teamImage" src={washRaas} alt="Team 5" onClick={() => window.open('https://youtu.be/ef5TEpz37Dg?si=UzKE8fWIxaJ9cdzS', '_blank')}/>
             <div className="overlay">
               <span className="overlayText">Team 5</span>
+              <span className="overlayTextName">Washington University Raas</span>
             </div>
           </div>
 
           <div className="teamFrameFive">
-            <img className="teamImage" src={uwRaas} alt="Team 5" onClick={() => window.open('https://youtu.be/9h78dRldiAQ?si=itSCPGE23od9lUTY', '_blank')}/>
+            <img className="teamImage" src={uwRaas} alt="Team 6" onClick={() => window.open('https://youtu.be/9h78dRldiAQ?si=itSCPGE23od9lUTY', '_blank')}/>
             <div className="overlay">
               <span className="overlayText">Team 6</span>
+              <span className="overlayTextName">University of Washington Raas</span>
             </div>
           </div>
 
@@ -76,6 +82,8 @@ function MediaPage() {
             <img className="teamImage" src={steelRaas} alt="Team 7" onClick={() => window.open('https://youtu.be/p3WNR5G1nW0?si=vf1iTF25-7zKfiVL', '_blank')}/>
             <div className="overlay">
               <span className="overlayText">Team 7</span>
+              <span className="overlayTextName">University of Pittsburgh</span>
+               <span className="overlayTextName">Steel City Raas</span>
             </div>
           </div>
 
@@ -83,6 +91,7 @@ function MediaPage() {
             <img className="teamImage" src={columbiaRaas} alt="Team 8" onClick={() => window.open('https://youtu.be/XEpvkG6oLJ0?si=JYbemU5Gy3u3FT2D', '_blank')}/>
             <div className="overlay">
               <span className="overlayText">Team 8</span>
+              <span className="overlayTextName">Columbia University Raas</span>
             </div>
           </div>
 
