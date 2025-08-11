174a7f86dc59e8c2cb65ac8b1090aa4c69b01c6d
sameeksha-mehrotra <saasha.mehrotra@gmail.com>
2025-08-10 21:20:02 -0500

    media changes

--- src/MediaPage.js
+++ src/MediaPage.js
@@ -20,58 +20,43 @@ function MediaPage() {
 
         <div className="teamsCenter">
 
-          <div className="teamFramePlacementFirst">
-            <div className="overlay">
-              <span className="overlayText">First Place</span>
-              <span className="overlayTextName">Georgia Tech</span>
-            </div>
+          <div className="teamFrameOther">
             <img className="teamImage" src={gtRaas} alt="First Place" onClick={() => window.open('https://youtu.be/BVLQoistZlQ?si=zdj55TL_Z-_bbHIH', '_blank')}/>
+            <span className="overlayText">First Place</span>
+            <span className="overlayTextName"> <br/> Georgia Tech</span>
           </div>
 
-          <div className="teamFramePlacement">
+          <div className="teamFrame">
             <img className="teamImage" src={taraas} alt="Second Place" onClick={() => window.open('https://youtu.be/aQQ4JB4apHk?si=583LOQBUinVUeoc9', '_blank')}/>
-            <div className="overlay">
-              <span className="overlayText">Second Place</span>
-              <span className="overlayTextName">University of Texas at Dallas</span>
-            </div>
+            <span className="overlayText">Second Place</span>
+            <span className="overlayTextName"><br/>University of Texas at Dallas</span>
           </div>
 
-          <div className="teamFrameFive">
+          <div className="teamFrame">
             <img className="teamImage" src={ucbRaas} alt="Third Place" onClick={() => window.open('https://youtu.be/uVIZPXv-hsI?si=i6_PyxDnKoTB9vEW', '_blank')}/>
-            <div className="overlay">
-              <span className="overlayText">Third Place</span>
-              <span className="overlayTextName">UC Berkeley</span>
-            </div>
+            <span className="overlayText">Third Place</span>
+            <span className="overlayTextName"><br/>UC Berkeley</span>
           </div>
-
         </div>
 
         <div className="teamsLeft">
 
           <div className="teamFrame">
-            <a href="https://www.youtube.com/watch?v=47rsMTNm95U&t=40s" target="_blank" rel="noreferrer"> 
-              <img className="teamImage" src={raasAM} alt="Team 4"/>
-            </a>
-            <div className="overlay">
-              <span className="overlayText">Team 4</span>
-              <span className="overlayTextName">Texas A&M</span>
-            </div>
+            <img className="teamImage" src={raasAM} alt="Team 4" onClick={() => window.open('https://www.youtube.com/watch?v=47rsMTNm95U&t=40s', '_blank')}/>
+            <span className="overlayText">Team 4</span>
+            <span className="overlayTextName"><br/>Texas A&M</span>
           </div>
 
           <div className="teamFrame">
             <img className="teamImage" src={washRaas} alt="Team 5" onClick={() => window.open('https://youtu.be/ef5TEpz37Dg?si=UzKE8fWIxaJ9cdzS', '_blank')}/>
-            <div className="overlay">
-              <span className="overlayText">Team 5</span>
-              <span className="overlayTextName">Washington University</span>
-            </div>
+            <span className="overlayText">Team 5</span>
+            <span className="overlayTextName"><br/>Washington University</span>
           </div>
 
-          <div className="teamFrameFive">
+          <div className="teamFrame">
             <img className="teamImage" src={uwRaas} alt="Team 6" onClick={() => window.open('https://youtu.be/9h78dRldiAQ?si=itSCPGE23od9lUTY', '_blank')}/>
-            <div className="overlay">
-              <span className="overlayText">Team 6</span>
-              <span className="overlayTextName">University of Washington</span>
-            </div>
+            <span className="overlayText">Team 6</span>
+            <span className="overlayTextName"><br/>University of Washington</span>
           </div>
 
         </div>
@@ -80,18 +65,14 @@ function MediaPage() {
 
           <div className="teamFrame">
             <img className="teamImage" src={steelRaas} alt="Team 7" onClick={() => window.open('https://youtu.be/p3WNR5G1nW0?si=vf1iTF25-7zKfiVL', '_blank')}/>
-            <div className="overlay">
-              <span className="overlayTextSteelNum">Team 7</span>
-              <span className="overlayTextNameSteel">University of Pittsburgh</span>
-            </div>
+             <span className="overlayText">Team 7</span>
+            <span className="overlayTextName"><br/>University of Pittsburgh</span>
           </div>
 
-          <div className="teamFrameFive">
+          <div className="teamFrameOtherC">
             <img className="teamImage" src={columbiaRaas} alt="Team 8" onClick={() => window.open('https://youtu.be/XEpvkG6oLJ0?si=JYbemU5Gy3u3FT2D', '_blank')}/>
-            <div className="overlay">
-              <span className="overlayText">Team 8</span>
-              <span className="overlayTextName">Columbia University</span>
-            </div>
+            <span className="overlayText">Team 8</span>
+            <span className="overlayTextName"><br/>Columbia University</span>
           </div>
 
         </div>
